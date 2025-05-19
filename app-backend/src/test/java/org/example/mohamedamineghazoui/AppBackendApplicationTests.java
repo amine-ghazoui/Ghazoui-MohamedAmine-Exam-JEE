f4c0236430d9a74b245760f22a6641f085843f37
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-05-19 09:03:44 +0100

    Initial commit: setup projet Angular/Spring Boot

---
+++ app-backend/src/test/java/org/example/mohamedamineghazoui/AppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mohamedamineghazoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
